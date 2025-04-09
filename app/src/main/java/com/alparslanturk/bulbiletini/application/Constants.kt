e18ee398d1927ad6542b87e04905ea7ec1392efd
Alp Arslantürk <alp.arslnturk@gmail.com>
2025-04-09 10:53:05 +0300

    Remove Constants.kt from Git tracking

--- app/src/main/java/com/alparslanturk/bulbiletini/application/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.alparslanturk.bulbiletini.application
-
-object Constants {
-    const val BASE_URL = "https://www.bulbiletini.com/"
-    const val ADMIN_USER = "admin"
-    const val ADMIN_PASS = "EjY1W9k7VyiGN8Vv"
-}
